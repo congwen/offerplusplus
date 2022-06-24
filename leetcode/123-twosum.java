e7ef32ee296f99a5fc5b28271f48875f53bdd3ac
丛文 <iamcongwen@gmail.com>
2022-06-24 14:55:48 +0000

    update

--- leetcode/123-twosum.java
+++ leetcode/123-twosum.java
@@ -1,5 +1,18 @@
-class TwoSum{
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //使用hashmap来保存历史数据
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            int tmp=target-nums[i];
+            if(map.containsKey(tmp)){//历史数据中如果存在差值，则有解
+                return new int[]{map.get(tmp),i};//返回结果
+            }else{
+                map.put(nums[i],i);//存入
+            }
+        }
+        return new int[]{-1,-1};//无结果
     }
 }
---
+++ leetcode/225-implement-stack-using-queues.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private Queue<Integer> a;
+    private Queue<Integer> b;
+    public MyStack() {
+        a=new LinkedList<>();
+        b=new LinkedList<>();
+
+    }
+    
+    public void push(int x) {
+        //当a队列不为空时，把a队列里所有的元素转移到b队列
+        while(!a.isEmpty()){
+            b.offer(a.poll());
+        }
+        //此时a队列为空，把x放入a队列
+        a.offer(x);
+        //再把b队列的里元素转移到a队列，x在a队列第一个，实现后进先出
+        while(!b.isEmpty()){
+            a.offer(b.poll());
+        }
+
+
+    }
+    
+    public int pop() {
+           return a.poll(); 
+    }
+    
+    public int top() {
+          return a.peek();  
+    }
+    
+    public boolean empty() {
+        return a.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
